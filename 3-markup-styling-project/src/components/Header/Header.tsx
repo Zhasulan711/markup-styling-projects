5f7a4597c8b1c82fb55d1b1bce25aa0b1f7fa771
Zhasulan Muratov <136080283+Zhasulan711@users.noreply.github.com>
2023-12-03 03:58:51 +0600

    Adding responsive design in Header.tsx

--- 3-markup-styling-project/src/components/Header/Header.tsx
+++ 3-markup-styling-project/src/components/Header/Header.tsx
@@ -2,17 +2,48 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="h-[95px] w-full bg-white flex flex-row space-x-[420px] pt-[55px] pl-[200px]">
+    <header
+      className="h-[95px] w-full bg-white flex flex-row pt-[55px] 
+      sm-min:space-x-[57px] sm:space-x-[114px] md:space-x-[171px] lg:space-x-[228px] xl:space-x-[285px] pc-sm:md:space-x-[340px] 
+      justify-center"
+    >
       <h1 className="text-[#757575] text-2xl font-medium">
         <span className="text-[#6F7171] font-semibold">Seven</span>Oaks
       </h1>
-      <nav className="flex flex-row  space-x-[65px]">
-        <Link href="/about" className="text-[#5B8429] transition-colors hover:text-[#0595BE]">ABOUT</Link>
-        <Link href="/about" className="text-[#5B8429] transition-colors hover:text-[#0595BE]">MEDIA</Link>
-        <Link href="/about" className="text-[#5B8429] transition-colors hover:text-[#0595BE]">EVENTS</Link>
-        <Link href="/about" className="text-[#5B8429] transition-colors hover:text-[#0595BE]">SHOP</Link>
-        <Link href="/about" className="text-[#5B8429] transition-colors hover:text-[#0595BE]">GIVE</Link>
+      <nav className="flex flex-row pt-[5px]
+        sm-min:space-x-[11px] sm:space-x-[22px] md:space-x-[33px] lg:space-x-[44px] xl:space-x-[55px] pc-sm:space-x-[65px]">
+        <Link
+          href="/about"
+          className="text-[#5B8429] transition-colors hover:text-[#0595BE]"
+        >
+          ABOUT
+        </Link>
+        <Link
+          href="/about"
+          className="text-[#5B8429] transition-colors hover:text-[#0595BE]"
+        >
+          MEDIA
+        </Link>
+        <Link
+          href="/about"
+          className="text-[#5B8429] transition-colors hover:text-[#0595BE]"
+        >
+          EVENTS
+        </Link>
+        <Link
+          href="/about"
+          className="text-[#5B8429] transition-colors hover:text-[#0595BE]"
+        >
+          SHOP
+        </Link>
+        <Link
+          href="/about"
+          className="text-[#5B8429] transition-colors hover:text-[#0595BE]"
+        >
+          GIVE
+        </Link>
       </nav>
     </header>
   );
 }
+
