886ce0422b931413c548896559b4ed21a938b1a1
Zhasulan Muratov <136080283+Zhasulan711@users.noreply.github.com>
2023-12-03 04:00:29 +0600

    Adding screens sizes in tailwind.config.ts

--- 3-markup-styling-project/tailwind.config.ts
+++ 3-markup-styling-project/tailwind.config.ts
@@ -18,6 +18,21 @@ const config: Config = {
         DEFAULT: "0 2px 4px var(--tw-shadow-color)",
         lg: "0 8px 16px var(--tw-shadow-color)",
       },
+      screens: {
+        "sm-min": "550px", // Phone
+        sm: "640px", // Tablet/Phone
+        md: "768px", // Tablet/Phone
+        lg: "1024px", // PC/Laptop
+        xl: "1280px", // PC/Laptop
+        "pc-sm": "1366px", // PC/Laptop
+        "pc-md": "1440px", // PC/Laptop
+        "2xl": "1536px", // PC/Laptop
+        "pc-16-9": "1600px", // PC/Laptop
+        "pc-full-hd": "1920px", // PC/Laptop
+        "pc-2k": "2560px", // PC/Laptop
+        "pc-ws": "3440px", // PC
+        "pc-4k": "3840px", // PC
+      },
     },
   },
   plugins: [
