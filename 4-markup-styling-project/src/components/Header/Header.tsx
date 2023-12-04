7abf86c9c2585fa1276c2b8cecdead7f5474692b
Zhasulan Muratov <136080283+Zhasulan711@users.noreply.github.com>
2023-12-05 03:49:04 +0600

    Adding responsive design in Header.tsx

--- 4-markup-styling-project/src/components/Header/Header.tsx
+++ 4-markup-styling-project/src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="w-full h-[75px] bg-[#204A52] flex flex-row justify-center pt-8
-    sm-min:space-x-[160px] sm:space-x-[250px] md:space-x-[410px] lg:space-x-[500px] xl:space-x-[600px] pc-sm:space-x-[750px] ">
+    sm-little:space-x-[110px] sm-min:space-x-[160px] sm:space-x-[250px] md:space-x-[410px] lg:space-x-[500px] xl:space-x-[600px] pc-sm:space-x-[750px] ">
       <h1 className="text-[#F5FCFC] text-xl">Site name</h1>
       <nav className="flex flex-row space-x-[30px] mt-1">
         <Link href="/Home" className="text-[#F5FCFC]">
