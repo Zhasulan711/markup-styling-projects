08fda29431be0bfd1c6e8c79362df405ed62e650
Zhasulan Muratov <136080283+Zhasulan711@users.noreply.github.com>
2023-12-05 03:48:44 +0600

    Adding responsive design in Footer.tsx

--- 4-markup-styling-project/src/components/Footer/Footer.tsx
+++ 4-markup-styling-project/src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="w-full bg-[#204A52] h-[130px] mt-[70px] text-center pt-[50px]">
       <aside>
-        <h5 className="text-[#E7EFE9]">
+        <h5 className="text-[#E7EFE9] sm-little:text-xs sm-min:text-sm">
           Copyright(c) website name{" "}
           <span className="text-[#F9BD2A]">
             .Designed by: <Link href="/more">www.alltemplateneeds.com</Link>
